752286574789347e4d75e965dc7af8426e38d3c7
1520118 <1520118@sala31321.fit.net>
2017-02-21 22:22:20 -0300

    Junior 1

--- src/br/edu/impacta/ads/AgendaApp.java
+++ src/br/edu/impacta/ads/AgendaApp.java
@@ -6,111 +6,109 @@ import java.util.Scanner;
 import static java.lang.System.out;
 
 public class AgendaApp {
-
 	private static Scanner entrada = new Scanner(System.in);
-	private static List<Contato> contatos = new ArrayList<>();
-
-	public static void main(String[] args) {
+	//private static List <Contato> contatos = new ArrayList<>();
+	private static IContatoDao dao = (IContatoDao) new ContatoDao();
+	
+	
+	public static void main (String[] args){
 		boolean sair = false;
-		while (!sair) {
+		while (!sair){
 			int opcao = apresentarMenuPrincipal();
-			switch (opcao) {
-			case 1:
-				inserirContato();
-				break;
-			case 2:
-				buscarContato();
-				break;
-			case 3:
-				sair = true;
-				break;
+			switch(opcao) {
+			case 1: inserirContato(); break;
+			case 2: buscarContato(); break;
+			case 3: sair = true; break;
+			default: out.println("ERRO: opção inválida!");
 			}
 		}
-		out.println("\nfim do programa");
+	    out.println("\nFim do programa!"); 
 	}
-
-	private static int apresentarMenuPrincipal() {
+	
+	private static int apresentarMenuPrincipal(){
 		boolean inteiro = false;
 		int opcao = 0;
-		while (!inteiro) {
+		while(!inteiro){
 			out.println("\nAGENDA TELEFÔNICA!");
 			out.println("(1) Inserir");
 			out.println("(2) Buscar");
 			out.println("(3) Sair");
-			out.print("Escolha uma opção: ");
+			out.println("Escolha uma opção: ");
 			String s = entrada.nextLine();
 			try {
 				opcao = Integer.parseInt(s);
 				inteiro = true;
-			} catch (Exception e) {
-				out.println("ERRO: opção deve ser um valor inteiro");
+			} catch(Exception e) {
+				out.println("ERRO: opção deve ser um valor inteiro!");
+				
 			}
 		}
 		return opcao;
+		
 	}
-
+	
 	private static void inserirContato() {
-		out.println("\nInserção de novo contato :");
+		out.println("\nINSERÇÃO DE NOVO CONTATO:");
 		String nome = lerNome();
 		String telefone = lerTelefone();
 		Contato c = new Contato(nome, telefone);
-		if (contatos.contains(c)) {
-			out.println("Este contato ja esta cadastrado");
-		} else {
-			contatos.add(c);
-			out.println("contato inserido");
-
+		//if (contatos.contains(c)){
+		if (dao.existe(c)){
+			out.println("Este contato já está cadastrado!");
+	
 		}
-
+		else {
+			//contatos.add(c);
+			dao.inserir(c);
+			out.println("Contato inserido!");
+		}
 	}
-
 	
 	private static String lerNome(){
 		boolean valido = false;
-		String nome ="";
-		while(!valido){
-			out.print("nome");
+		String nome = "";
+		while (!valido){
+			out.print("Nome: ");
 			nome = entrada.nextLine();
-			if(nome.length() == 0 || nome.length()>200){
-				out.println("erro nome doe tamanho invalido");
-			}else{valido =true;		}
-		}
+			if (nome.length()==0 || nome.length()>200){
+				out.println("ERRO: nome de tamanho inválido!");
+			}else{ valido = true;}
+			} 
 		return nome;
-	}
-
-
+		}
 	private static String lerTelefone(){
 		boolean valido = false;
-		String telefone ="";
-		while(!valido){
-			out.print("nome");
+		String telefone = "";
+		while (!valido){
+			out.print("Telefone: ");
 			telefone = entrada.nextLine();
-			if(telefone.length() == 0 || telefone.length()>200){
-				out.println("erro telefone do tamanho invalido");
-			}else{valido =true;		}
-		}
+			if (telefone.length()==0 || telefone.length()>25){
+				out.println("ERRO: telefone de tamanho inválido!");
+			}else{ valido = true;}
+			} 
 		return telefone;
+	
 	}
-
-
-	private static void buscarContato() {
-		out.println("\nBUSCA DE CONTATOS:");
+	
+	private static void buscarContato(){
+		out.println("\nBUSCA CONTATOS: ");
 		String nome = lerNome();
-		List<Contato> resultado = new ArrayList<>();
-		for (Contato c: contatos){
-			if (nome.equals(c.getName())) {
+		/*List<Contato> resultado = new ArrayList<>();
+		for(Contato c: contatos){
+			if (nome.equals(c.getNome())) {
 				resultado.add(c);
 			}
-		}
-		if (resultado.size() == 0) {
+		}*/
+		List<Contato> resultado = dao.buscar(nome);
+		if (resultado.size() == 0){
 			out.println("Não há contato com este nome!");
-		} else {
+		}else{
 			out.println("\nResultado da busca:");
-			for(Contato c: resultado) {
+			for (Contato c: resultado){
 				out.println(c);
 			}
 		}
+		
 	}
 	
 }
-
