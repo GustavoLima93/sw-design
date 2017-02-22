72098110500507427e9f6277f04a37cd28097b96
1520118 <1520118@sala31321.fit.net>
2017-02-21 22:02:21 -0300

    Versao Amadora

---
+++ src/br/edu/impacta/ads/Contato.java
@@ -0,0 +1,62 @@
+package br.edu.impacta.ads;
+
+public class Contato {
+	private String name;
+	private String telefone;
+	public Contato() {
+		super();
+	}
+	public Contato(String name, String telefone) {
+		super();
+		this.name = name;
+		this.telefone = telefone;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	@Override
+	public String toString() {
+		return "Contato [name=" + name + ", telefone=" + telefone + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (telefone == null) {
+			if (other.telefone != null)
+				return false;
+		} else if (!telefone.equals(other.telefone))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
